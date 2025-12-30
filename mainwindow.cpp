90e64bc3072ebbfa37b029a2ca12a5792d78c513
Qq-help <497753908@qq.com>
2025-12-30 16:19:14 +0800

    docs: 完善关键代码注释，补充数据库字段说明

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,7 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     // 初始化并连接数据库
     if (DatabaseManager::instance().openDb()) {
-        // 测试插入一条假数据
+        // 测试插入一条测试数据
         DatabaseManager::instance().insertData(25.5, 60.2, 500, "待机");
     }
 }
